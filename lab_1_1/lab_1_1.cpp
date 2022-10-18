eac9211217a27dbae8aa0150437cbeff81f91100
Your Name <you@example.com>
2022-10-18 17:00:14 +0200

    zad_podstawowe

--- lab_1_1/lab_1_1.cpp
+++ lab_1_1/lab_1_1.cpp
@@ -1,20 +1,51 @@
-// lab_1_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
 int main()
-{
-    std::cout << "Hello World!\n";
+{   
+    setlocale(LC_ALL, "");
+
+    double a, b, c;
+
+    cout << "Podaj a" << endl; cin >> a;
+    cout << "Podaj b" << endl; cin >> b;
+    cout << "Podaj c" << endl; cin >> c;
+
+
+    if (a != 0) {
+        double delta = b * b - 4 * a * c;
+        if (delta > 0) {
+            double sqrt_delta = sqrt(delta);
+            
+            double x1, x2;
+            x1 = (-b - sqrt_delta) / (2 * a);
+            x2 = (-b + sqrt_delta) / (2 * a);
+
+            cout << "Dwa rozwi¹zania (a=/=0, delta > 0): x1 = " << x1 << ", x2 = " << x2;
+        }
+        else if (delta == 0) {
+            double x = -b / (2 * a);
+            cout << "Jedno rozwi¹zanie (a=/=0, delta = 0): x1 = x2 = " << x << endl;
+        }
+        else {
+            cout << "Brak rozwi¹zañ (a=/= 0, delta < 0): x:{}";
+        }
+    }
+    else {
+        if (b != 0) {
+            //liniowe bx + c
+            double x0 = -c / b;
+            cout << "Jedno rozwi¹zanie (a=0, b=/=0): x0 = " << x0 << endl;
+        }
+        else if (c != 0) {
+            cout << "Brak rozwi¹zañ (a= 0, b=0, c=/=0): x:{}" << endl;
+        }
+        else {
+            cout << "Nieskoñczenie wiele rozwi¹zañ (a=0, b=0, c=0): x:R" << endl;
+        }
+    }
+    
+    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
